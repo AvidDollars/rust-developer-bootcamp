a7cc6f24a8c9df34c44803b862910a7b01c10798
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2023-10-19 22:31:13 +0200

    [HOMEWORK - lecture 05]: add new variant into 'AppError' enum

--- lecture_05-17_10/src/custom_errors.rs
+++ lecture_05-17_10/src/custom_errors.rs
@@ -1,20 +1,18 @@
 use std::fmt::{Debug, Display, Formatter, Result};
 
-//#[derive(Debug)]
 pub enum AppError {
     MissingCliArgument,
     InvalidCliArgument { message: String },
+    OnOutput(String),
 }
 
-// IS THERE A WAY TO GET RID OF DUPLICATE CODE?
-// BOTH "Debug" & "Display" HAVE THE SAME IMPLEMENTATION
 impl Debug for AppError {
     fn fmt(&self, f: &mut Formatter) -> Result {
         use AppError::*;
 
         let message = match self {
             MissingCliArgument => "Missing CLI argument.",
-            InvalidCliArgument { message } => message,
+            InvalidCliArgument { message } | OnOutput(message) => message,
         };
 
         write!(f, "{}", message)
@@ -23,13 +21,6 @@ impl Debug for AppError {
 
 impl Display for AppError {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        use AppError::*;
-
-        let message = match self {
-            MissingCliArgument => "Missing CLI argument.",
-            InvalidCliArgument { message } => message,
-        };
-
-        write!(f, "{}", message)
+        todo!();
     }
 }
