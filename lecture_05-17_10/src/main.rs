7a8f789ef816d131328f19e7521b889bd5294d64
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2023-10-19 20:46:22 +0200

    [HOMEWORK - lecture 05]: main.rs

--- lecture_05-17_10/src/main.rs
+++ lecture_05-17_10/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    
+#![deny(unreachable_code, unreachable_patterns)]
+// TODO! remove after homework is done
+#![allow(unused)]
+
+mod custom_errors;
+mod helpers;
+
+use custom_errors::AppError;
+use helpers::*;
+use std::{env, io, process};
+
+fn main() -> Result<(), AppError> {
+    // GETTING CLI ARG
+    let cli_arg = env::args()
+        .only_first_provided()
+        .ok_or(AppError::MissingCliArgument)?; //.unwrap_or_else(||{eprintln!("{}",AppError::MissingCliArgument);process::exit(1);});
+
+    // GETTING TEXT TRANSFORMER
+    let text_transformer = TextTransformer::new(&cli_arg)?;
+
+    Ok(())
 }
