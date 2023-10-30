f3889060cab564b4e55301e925583d1bf38c0a19
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2023-10-30 21:42:15 +0100

    add 'main.rs' file

--- lecture_07-24_10/src/main.rs
+++ lecture_07-24_10/src/main.rs
@@ -1,5 +1,19 @@
-#![deny(unreachable_code, unreachable_patterns)] // TODO: unused
+#![deny(unreachable_code, unreachable_patterns, unused)]
 
-fn main() {
-    
+use std::any::Any;
+
+pub mod commands;
+pub mod csv_processing;
+pub mod custom_errors;
+pub mod extensions;
+pub mod spawn_thread;
+
+fn main() -> Result<(), Box<dyn Any + Send>> {
+    let (thread_for_processing, sender) = spawn_thread::processing();
+    let thread_for_receiving_input = spawn_thread::input_receiver(sender);
+
+    thread_for_processing.join()?;
+    thread_for_receiving_input.join()?;
+
+    Ok(())
 }
