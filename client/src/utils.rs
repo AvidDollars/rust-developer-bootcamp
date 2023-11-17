92ec691e82c6ee2b8a9bef65ad4d3a7b333f07ee
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2023-11-17 22:08:36 +0100

    refactor client crate

--- client/src/utils.rs
+++ client/src/utils.rs
@@ -1,6 +1,11 @@
 use std::fs;
 use std::io::{self, ErrorKind};
 
+use shared::message::Message;
+use shared::utils::new_thread;
+
+use std::sync::mpsc::{self, Receiver};
+
 pub fn create_missing_folders(paths: &[&str]) -> io::Result<()> {
     for path in paths {
         let folder_creation = match fs::create_dir(path) {
@@ -16,4 +21,33 @@ pub fn create_missing_folders(paths: &[&str]) -> io::Result<()> {
         }
     }
     Ok(())
+}
+
+pub fn spawn_stdin_channel() -> Receiver<Message> {
+    let (sender, receiver) = mpsc::channel::<Message>();
+
+    new_thread!({
+        #[cfg(debug_assertions)]
+        println!("INFO: spawned stdin channel");
+
+        loop {
+            let mut stdin_line = String::new();
+
+            match io::stdin().read_line(&mut stdin_line) {
+                Ok(_) => (),
+                Err(error) => {
+                    eprintln!("unable to read from stdin: {error}");
+                    continue;
+                }
+            }
+
+            match Message::try_from(stdin_line.trim()) {
+                Ok(message) => sender.send(message).unwrap_or_else(|error| {
+                    eprintln!("unable to send via channel: {error}");
+                }),
+                Err(error) => eprintln!("cannot create message from the input: {}", error),
+            };
+        }
+    });
+    receiver
 }
